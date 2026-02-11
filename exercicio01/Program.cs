40c81845db37da340b89748b313d46c9c1f63eb6
pedro <ph3669023@gmail.com>
2026-02-11 13:50:08 -0300

    chore: estrutura inicial do projeto e exercicio01

---
+++ exercicio01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace exercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A,B,C,soma=0;
+
+            Console.WriteLine("Digite o valor de A: ");
+            A = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor de B: ");
+            B = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor de C: ");
+            C = int.Parse(Console.ReadLine());
+
+            soma = A+B;
+            Console.WriteLine("A soma de A e B é: "+ soma);
+
+            if(soma<C)
+            {
+                Console.WriteLine(soma + " é menor que " + C);
+            }
+            else Console.WriteLine(soma + " não é menor que " + C);
+        }
+    }
+}
