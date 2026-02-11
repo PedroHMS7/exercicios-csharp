bba0788419640dc08912b7270c6c6a3a60d0dbae
pedro <ph3669023@gmail.com>
2026-02-11 15:36:02 -0300

    feat: implementação dos exercícios 2 e 3 em c#

---
+++ exercicio02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace exercicio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.WriteLine("Informe um número: ");
+            num = int.Parse(Console.ReadLine());
+
+            if(num == 0)
+            {   
+                Console.WriteLine("O número 0 é par e neutro (não é positivo nem negativo)");
+            }
+            else if(num % 2 == 0 && num > 0)
+            {
+                Console.WriteLine($"O número {num} é par e positivo");
+            }
+            else if(num % 2 == 0 && num < 0)
+            {
+                Console.WriteLine($"O número {num} é par e negativo");
+            }
+            else if(num % 2 != 0 && num > 0)
+            {
+                Console.WriteLine($"O número {num} é ímpar e positivo");
+            }
+            else Console.WriteLine($"O número {num} é ímpar e negativo");
+        }
+    }
+}
---
+++ exercicio03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace exercicio03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A,B,C;
+
+            Console.WriteLine("Digite o valor de A: ");
+            A = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor de B: ");
+            B = int.Parse(Console.ReadLine());
+
+            if(A==B)
+            {
+                C = A+B;
+                Console.WriteLine($"Como A e B são iguais o valor de C é a soma entre eles: {C}");
+            }
+            else
+            {
+                C = A*B;
+                Console.WriteLine($"Como A e B são diferentes o valor de C é a multiplicação entre eles: {C}");
+            }
+        }
+    }
+}
