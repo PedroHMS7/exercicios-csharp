94e805bf029edbdf2da9c7647b457404fe4a4960
pedro <ph3669023@gmail.com>
2026-02-16 14:08:17 -0300

    feat: implementação dos exercícios 7 e 8 em c#

---
+++ exercicio07/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace exercicio07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           bool x,y;
+
+           Console.WriteLine("Digite o valor lógico de X (TRUE/FALSE): ");
+           x = bool.Parse(Console.ReadLine());
+
+           Console.WriteLine("Digite o valor lógico de Y (TRUE/FALSE): ");
+           y = bool.Parse(Console.ReadLine());
+
+           if(x == y)
+           {
+                Console.WriteLine("Os dois valores são iguais (TRUE ou FALSE)");
+           }
+           else Console.WriteLine("Os dois valores são diferentes");
+        }
+    }
+}
---
+++ exercicio08/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace exercicio08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            Console.WriteLine("Digite o valor de A: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor de B: ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor de C: ");
+            c = int.Parse(Console.ReadLine());
+
+            if (a > b && a > c)
+            {
+                if (b > c)
+                {
+                    Console.WriteLine($"{a} - {b} - {c}");
+                }
+                else Console.WriteLine($"{a} - {c} - {b}");
+            }
+            else if (b > a && b > c)
+            {
+                if (a > c)
+                {
+                    Console.WriteLine($"{b} - {a} - {c}");
+                }
+                else Console.WriteLine($"{b} - {c} - {a}");  
+            }
+            else
+            {
+                if(b>a)
+                {
+                    Console.WriteLine($"{c} - {b} - {a}");
+                }
+                else  Console.WriteLine($"{c} - {a} - {b}");
+            }
+        }
+    }
+}
