2fb4faae7bd612bbfd077f6259dedb3e2c881e49
pedro <ph3669023@gmail.com>
2026-02-19 14:53:01 -0300

    feat: implementação dos exercícios 13 e 14 em c#

---
+++ exercicio13/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace exercicio13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual o seu nome?");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Qual a sua idade?");
+            int idade = int.Parse(Console.ReadLine());
+
+            if(idade>=18)
+            {
+                Console.WriteLine($"{nome} é maior de idade com {idade} anos");
+            }else{
+                Console.WriteLine($"{nome} é menor de idade com {idade} anos");  
+            }
+
+        }
+    }
+}
---
+++ exercicio14/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace exercicio14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o valor de A:");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o valor de B:");
+            int b = int.Parse(Console.ReadLine());
+
+            int novoA = b;
+            int novoB = a;
+
+            Console.WriteLine($"Novo valor de A: {novoA}");
+            Console.WriteLine($"Novo valor de B: {novoB}");
+        }
+    }
+}
